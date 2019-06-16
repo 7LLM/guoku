0b5e2ed9a55b952c7503c732a96ef021afcd6bdd
yingzi <2508743765@qq.com>
2019-06-16 11:03:21 +0800

    新增购物车页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import Home from '@/pages/home/home'
 import JoinIn from '@/pages/joinIn/joinIn'
 import Mall from '@/pages/mall/mall'
-
+import Shoppcar from '@/pages/mall/shoppcar'
 Vue.use(Router)
 
 export default new Router({
@@ -16,7 +16,8 @@ export default new Router({
     },
     {path:"/home",component:Home},
     {path:"/joinIn",component:JoinIn},
-    {path:"/Mall",component:Mall}
+    {path:"/Mall",component:Mall},
+    {path:"/shoppcar",component:Shoppcar}
     
   ]
 })
